3662b39cd5b88c83cf74e7a7a60698b10f575a05
Kemi Omo <kemiomon@gmail.com>
2021-08-05 16:05:48 -0500

    Initial Commit

---
+++ finalApp/eighthquestionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  eighthquestionViewController.swift
+//  finalApp
+//
+//  Created by Scholar on 8/5/21.
+//
+
+import UIKit
+
+class eighthquestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
